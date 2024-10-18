45d91bcb9bc9de1e3e50c5e76e7301ed253693e4
Lucas Thainan <lucasthainan.cm@gmail.com>
2024-10-17 21:42:38 -0300

    feat: configure mysql typeorm

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { EntregadoresModule } from './entregadores/entregadores.module'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'admin',
+      database: 'lastmile',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true
+    }),
+    EntregadoresModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
